202c9d651852875342615fb9ca81d221eaa25a13
Mary <omota4fidelity@gmail.com>
2023-09-13 15:11:52 +0100

    completed

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,25 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <div>
+    <h1> Movies Page </h1>
+  {movies.map((movie, index) => {
+      return (
+      <div key={index}>
+        <h3>Name:{movie.title} </h3>
+        <h5>Time:{movie.time} </h5>
+        <h5>Genres: </h5>
+      <ul>
+        {movie.genres.map((genre, index) => {
+         return <li key={index}>{genre}</li>
+        })}
+      </ul>
+        </div>
+      )
+    })}
+ </div>
+ );
 }
 
 export default Movies;
